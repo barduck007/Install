dfc923e20f5d0ce47fd3c6e6b817b91745be3fef
Nicolas Mignard <nicolas.mignard@overblog.com>
2022-03-29 17:59:13 +0200

    Remove old unused apps

--- OSX/install_brew.sh
+++ OSX/install_brew.sh
@@ -22,15 +22,12 @@ brew install yarn
 brew install dropbox
 brew install spotify
 brew install slack
-brew install rambox # Skype, Whatsapp, messenger, ...
 brew install iterm2
 brew install vlc
 brew install atom
 brew install google-chrome
 brew install phpstorm
 brew install dashlane
-brew install virtualbox
-brew install filezilla
 
 ## Usage stats network / cpu /ram
 brew install yujitach-menumeters
@@ -39,12 +36,3 @@ brew install tunnelblick
 ## SSH tunnel manager
 brew install ssh-tunnel-manager
 
-#brew cask install cloud
-#brew cask install hammerspoon
-#brew cask install grandperspective
-#brew cask install limechat
-#brew cask install postico
-# required by some updates for brew
-#brew cask install xquartz
-#brew cask install sequel-pro
-#brew cask install cyberduck
